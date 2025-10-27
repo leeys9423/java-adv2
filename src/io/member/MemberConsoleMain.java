7ef37ac5edd1a65b96a74f9519678f90d96ee960
leeys9423 <leeys9423@gmail.com>
2025-10-27 22:57:55 +0900

    회원 관리 예제3 - DataStream

--- src/io/member/MemberConsoleMain.java
+++ src/io/member/MemberConsoleMain.java
@@ -1,5 +1,6 @@
 package io.member;
 
+import io.member.impl.DataMemberRepository;
 import io.member.impl.FileMemberRepository;
 import io.member.impl.MemoryMemberRepository;
 
@@ -9,7 +10,8 @@ import java.util.Scanner;
 public class MemberConsoleMain {
 
 //    private static final MemberRepository repository = new MemoryMemberRepository();
-    private static final MemberRepository repository = new FileMemberRepository();
+//    private static final MemberRepository repository = new FileMemberRepository();
+    private static final MemberRepository repository = new DataMemberRepository();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -30,6 +32,7 @@ public class MemberConsoleMain {
                     break;
                 case 3:
                     System.out.println("프로그램을 종료합니다.");
+                    return;
                 default:
                     System.out.println("잘못된 선택입니다. 다시 입력하세요.");
             }
