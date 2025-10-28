253f5d1301678a7795a450b9fceadaecb620abb2
leeys9423 <leeys9423@gmail.com>
2025-10-28 21:04:28 +0900

    File

---
+++ src/io/file/OldFileMain.java
@@ -0,0 +1,27 @@
+package io.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class OldFileMain {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("temp/example.txt");
+        File directory = new File("temp/exampleDir");
+
+        System.out.println("File exists: " + file.exists());
+
+        boolean created = file.createNewFile();
+        System.out.println("File created: " + created);
+
+        boolean dirCreated = directory.mkdir();
+        System.out.println("Directory created: " + dirCreated);
+
+//        boolean deleted = file.delete();
+//        System.out.println("File deleted: " + deleted);
+
+        System.out.println("Is file: " + file.isFile());
+        System.out.println("Is directory: " + directory.isDirectory());
+        System.out.println("File name: " + file.getName());
+    }
+}
